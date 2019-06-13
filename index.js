c7c263d207962fbd4403c31969a47543af3f1971
Jill Casey <jillcaseycodes@gmail.com>
2019-06-13 16:07:31 -0700

    added easter egg

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("For your curiosity, I have included a surprise poem. If you like it, consider it a reward. If not, well then, these aren't the droids you are looking for."); 
+
+const acrostic = function(words){
+  return codedPoetry;
+}; 
+
+
+const codedPoetry = 
+alert("Happiness is coding, executing"+"\\n"+"Thougt provoking ideas."+"\\n"+"Making it work elegantly,"+"\\n"+"Loving the process!"+"\\n"+" "+"\\n"+"Creativity is"+"\\n"+"Salve for the"+"\\n"+"Soul."+"\\n"+" "+"\\n"+"Joy in finding the working"+"\\n"+"Solutions."+"\\n"+""+"\\n"+"Recognizing"+"\\n"+"Each others' strengths"+"\\n"+"And"+"\\n"+"Collaborating  -"+"\\n"+"Thoughfully"+"\\n"+" "+"\\n"+"Noticing the dawn of"+"\\n"+"Opportunities to"+"\\n"+"Discover something new: "+"\\n"+"Emergence of inspriration."+"\\n"+" "+"\\n"+" Searching with well designed"+"\\n"+"Queries, communicating results, "+"\\n"+"Leaving no stone un-turned.");
